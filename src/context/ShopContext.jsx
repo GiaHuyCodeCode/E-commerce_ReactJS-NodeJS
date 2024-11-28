6dbf39e2faccb7291116b29fa36c44182d7bcdd2
GiaHuyCodeCode <115602753+GiaHuyCodeCode@users.noreply.github.com>
2024-11-28 23:18:51 +0700

    FE: Cart Page + Payment Method

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
@@ -10,6 +11,7 @@ const ShopContextProvider = (props) => {
   const [showSearch, setShowSearch] = useState(false);
   // State to hold the cart items
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
 
   // Function to add an item to the cart
   const addToCart = async (itemId, size) => {
@@ -75,6 +77,21 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   };
 
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+    return totalAmount;
+  };
+
   // Effect to log the cart items whenever they change
   // useEffect(() => {
   //   console.log(cartItems);
@@ -92,6 +109,8 @@ const ShopContextProvider = (props) => {
     addToCart,
     getCartCount,
     updateQuantity,
+    getCartAmount,
+    navigate,
   };
 
   return (
